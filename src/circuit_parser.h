0e7f181ebf628043c4e85bfb8e88c2e5a51dc028
JMGK <jmgk77@gmail.com>
2022-03-29 12:05:12 -0300

    add alias command

--- src/circuit_parser.h
+++ src/circuit_parser.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <map>
+#include <regex>
 #include <string>
 #include <utility>
 #include <vector>
@@ -24,6 +25,8 @@ class CircuitFileParser
   bool parseDontCare(std::string& lineStr);
   bool parseConnection(std::string& lineStr);
   void checkConnectionPoint(const ConnectionPoint& connectionPoint);
+  bool parseAlias(std::string& lineStr);
   Layout& layout_;
   Via offset_;
+  std::vector<std::pair<std::string, std::string> > aliases_;
 };
